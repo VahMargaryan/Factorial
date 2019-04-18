1ad1de31782eece7fdfa700ecfb226ab89ad5203
VahMargaryan <47668983+VahMargaryan@users.noreply.github.com>
2019-04-18 12:15:08 +0400

    Update fact.php

--- fact.php
+++ fact.php
@@ -1,5 +1,6 @@
 <?php
-if ($_POST) {
+if ($_POST) 
+{
     $fact = 1;
     $number = $_POST['n'];
     if (is_numeric($number))
@@ -10,16 +11,16 @@ if ($_POST) {
             {
                 $fact = $fact * $i;
             }
-            echo  "<h3 id='num' style='color:white'>Fact. of $number is " . " " . $fact . "<br>";}
+            echo  "<h3 id='num' style='color:white'> Fact. of $number is " . " " . $fact . "<br>";
+        }
         else
         {
-            echo "<h3 style='color:white'>Number must be greater or equal zero";
+            echo "<h3 style='color:white'> Number must be greater or equal zero";
         }
-    }
+     }
     else
     {
         echo "<h3 style='color:white'>Input Number Only";
     }
-
 }
 ?>
